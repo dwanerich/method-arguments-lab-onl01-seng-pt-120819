e25904ad4cb80bfdf50404e609985c7953acd75b
dwane richards <dwanerich@gmail.com>
2019-12-10 00:55:25 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+def name_greeting(name)
+  
+  puts "hello #{name} welcome to Flatiron"
+end
+
+name_greeting("dwane")
+
+def name_language(name, language)
+  
+  puts "hi #{name}!, learing #{language} will be so fun."
+end
+
+name_language("dwane", "ruby")
